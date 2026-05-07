2a5b2388f962053e75ddd9ee5f650c8ac041d5f2
Rieksts Zigmunds Andrejs (EXT Accenture) <zigmunds.a.rieksts@accenture.com>
2026-05-07 16:34:40 +0300

    [refactor]: improving code

--- src/main/java/com/deviceshop/customer/dto/CustomerRequest.java
+++ src/main/java/com/deviceshop/customer/dto/CustomerRequest.java
@@ -1,16 +1,11 @@
 package com.deviceshop.customer.dto;
 
-import org.platformlambda.core.serializers.SimpleMapper;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-import java.util.Map;
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerRequest {
-
     public String name;
     public String email;
-
-    @SuppressWarnings("unchecked")
-    public Map<String, Object> toMap() {
-        return SimpleMapper.getInstance().getMapper().readValue(this, Map.class);
-    }
 }
--- src/main/java/com/deviceshop/customer/models/Customer.java
+++ src/main/java/com/deviceshop/customer/models/Customer.java
@@ -6,9 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     @Id
@@ -20,37 +28,4 @@ public class Customer {
 
     @Column(nullable = false, unique = true)
     private String email;
-
-    public Customer() {
-    }
-
-    public Customer(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
